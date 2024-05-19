da16681ab7481b8aa3bbf2944b35ceff50d97f03
aaron-33 <aaron.lls.dev@gmail.com>
2024-05-19 15:27:17 -0600

    feat: add json props

--- src/helpers/properties.js
+++ src/helpers/properties.js
@@ -2,16 +2,15 @@ export const properties = [
   {
     id: 1,
     name: 'Apartments at Palm Bay',
-    location: 'Palm Bay, FL',
+    location: 'Palm Bay FL',
     image: '/home/s-01.jpg',
     status: 'in-process',
     slug: 'apartments-at-palm-bay',
-    description: 'Coming soon to central Florida is Townhomes at Powell. Residential property adjacent to the villages.  A new residential townhome community with 132 units featuring luxury amenities including a resort style pool & Clubhouse area.',
-    size: 132,
+    description: 'TBA',
+    size: 90,
     unit: 'units',
     details: [
-      'Apartments',
-      'Townhomes',
+      'TBA',
     ],
     gallery: [
       ''
@@ -22,16 +21,16 @@ export const properties = [
   },
   {
     id: 2,
-    name: 'Apartments at Palm Bay',
-    location: 'Palm Bay, FL',
+    name: 'Crela Spring Hill',
+    location: 'Springhill FL',
     image: 'https://soyarquitectura.mx/wp-content/uploads/2021/09/casas-de-lujo-blogc10.jpg',
     status: 'in-process',
-    slug: 'apartments-at-palm-bay',
-    description: 'Coming soon to central Florida is Townhomes at Powell. Residential property adjacent to the villages.  A new residential townhome community with 132 units featuring luxury amenities including a resort style pool & Clubhouse area.',
-    size: 132,
-    unit: 'units',
+    slug: 'crela-spring-hill',
+    description: 'TBA',
+    size: 162,
+    unit: 'Townhomes',
     details: [
-      'Apartments',
+      'TBA',
       'Townhomes',
     ],
     gallery: [
@@ -43,11 +42,31 @@ export const properties = [
   },
   {
     id: 3,
-    name: 'Apartments at Palm Bay',
-    location: 'Palm Bay, FL',
+    name: 'Cottages at Lake Hamilton',
+    location: 'Winter Haven FL',
     image: 'https://realestatemarket.com.mx/images/2023/08-agosto/01-08/Ciudades-de-EU-con-mayor-cantidad-de-casas-de-lujo-en-venta-II.jpeg',
     status: 'in-process',
-    slug: 'apartments-at-palm-bay',
+    slug: 'cottages-at-lake-hamilton',
+    description: 'TBA',
+    size: 200,
+    unit: 'units',
+    details: [
+      'TBA',
+    ],
+    gallery: [
+      ''
+    ],
+    tags: [
+      'multifamily',
+    ],
+  },
+  {
+    id: 4,
+    name: 'Townhomes at Powell',
+    location: 'Wildwood FL',
+    image: 'https://realestatemarket.com.mx/images/2023/08-agosto/01-08/Ciudades-de-EU-con-mayor-cantidad-de-casas-de-lujo-en-venta-II.jpeg',
+    status: 'in-process',
+    slug: 'townhomes-at-powell',
     description: 'Coming soon to central Florida is Townhomes at Powell. Residential property adjacent to the villages.  A new residential townhome community with 132 units featuring luxury amenities including a resort style pool & Clubhouse area.',
     size: 132,
     unit: 'units',
