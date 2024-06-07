71b370d950292dea38c79704cf9aa295049d482a
aaron-33 <aaron.lls.dev@gmail.com>
2024-06-06 22:46:52 -0600

    feat: add suspense

--- src/app/portfolio/mx/page.js
+++ src/app/portfolio/mx/page.js
@@ -1,11 +1,14 @@
+import { Suspense } from "react";
 import Menu from "@/components/Menu/Menu";
 import Portfolio from "@/components/Portfolio/Portfolio";
 
 export default function portfolio() {
 
   return (
-    <Menu>
-      <Portfolio isMX />
-    </Menu>
+    <Suspense>
+      <Menu>
+        <Portfolio isMX />
+      </Menu>
+    </Suspense>
   );
 }
