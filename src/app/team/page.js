1815163ac74e5674eb706008faff32fc51c2adab
aaron-33 <aaron.lls.dev@gmail.com>
2024-06-14 01:33:47 -0600

    feat: fix text team

--- src/app/team/page.js
+++ src/app/team/page.js
@@ -16,11 +16,6 @@ export default function Team() {
         <TeamSlider />
 
         <div className="team-content">
-          <div className="light">
-            <p><strong>Alan Benenson</strong>, co-founder and principal of A.S. Development, has a rich background in real estate investment and land entitlements. A University of Florida alumnus with a B.S. in Building Construction, he excels in land acquisition, project valuation, and development across various asset classes.</p>
-            <br />
-            <p>Alan’s career includes co-founding Universal Storage Group and founding Futurex Construction, showcasing his versatility in commercial and residential projects. A licensed general contractor and real estate developer, he has been pivotal in acquiring and developing over 3 million square feet of real estate.</p>
-          </div>
           <div className="strong">
             <p>Agador Spartacus Development is a regional, professionally-run, privately-owned company that has been doing business in the U.S. and Mexico.</p>
             <br />
