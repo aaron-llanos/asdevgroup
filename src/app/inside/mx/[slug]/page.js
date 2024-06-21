3155c00311a75f41e596b49d38e6af50b286c749
aaron-33 <aaron.lls.dev@gmail.com>
2024-06-21 17:13:39 -0600

    static slug

--- src/app/inside/mx/[slug]/page.js
+++ src/app/inside/mx/[slug]/page.js
@@ -1,4 +1,9 @@
 import Inside from "@/components/Inside/page";
+import { propertiesMX } from "@/helpers/properties";
+
+export async function generateStaticParams() {
+  return propertiesMX.map((property) => ({ slug: property.slug }));
+}
 
 export default function Home({ params }) {
   return (
--- src/app/inside/us/[slug]/page.js
+++ src/app/inside/us/[slug]/page.js
@@ -1,4 +1,9 @@
 import Inside from "@/components/Inside/page";
+import { properties } from "@/helpers/properties";
+
+export async function generateStaticParams() {
+  return properties.map((property) => ({ slug: property.slug }));
+}
 
 export default function Home({ params }) {
   return (
