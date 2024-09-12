0c4eb6a75dc1f360677dd85289fa52b4856a3b4d
LuisEduardoHernandez <luis.hdz9720@gmail.com>
2024-09-11 22:19:02 -0600

    Se agrega headmap

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,10 @@ export default function RootLayout({ children }) {
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','G-N9XH694YBL');
+
+              (function(h,o,t,j,a,r){         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};         h._hjSettings={hjid:5132185,hjsv:6};         a=o.getElementsByTagName('head')[0];         r=o.createElement('script');r.async=1;         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;         a.appendChild(r);     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
             `,
+
           }}
         />
       </head>
@@ -35,6 +38,8 @@ export default function RootLayout({ children }) {
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
+
+    
         {children}
       </body>
     </html>
