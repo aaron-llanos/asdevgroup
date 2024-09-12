b33d4a83fbee8fd9e06c6697398a9782fa030178
LuisEduardoHernandez <luis.hdz9720@gmail.com>
2024-09-11 21:51:01 -0600

    Se agregan cambios de responsive y tag google

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import {GoogleTagManager} from '@next/third-parties/google';
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -13,8 +11,32 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-       <GoogleTagManager gtmId="G-N9XH694YBL" />
-      <body className={inter.className}>{children}</body>
+      <head>
+        {/* Insertar el script de Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','G-N9XH694YBL');
+            `,
+          }}
+        />
+      </head>
+      <body className={inter.className}>
+        {/* Insertar iframe de Google Tag Manager */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=G-N9XH694YBL"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
